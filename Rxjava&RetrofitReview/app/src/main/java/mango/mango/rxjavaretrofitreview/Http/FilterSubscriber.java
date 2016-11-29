031f4ecbcbe80a36aebe03b7227696e0fb585009
Little Mango <244261996@qq.com>
2016-11-29 12:04:06 +0800

    增加一种异常分类
    模拟APP使用过程中出现的常见异常，将异常描述转成自定义的错误描述。并且由于是统一处理，所以可以选择将目标异常上传至服务器，做好bug的收集，方
    便处理。

--- Rxjava&RetrofitReview/app/src/main/java/mango/mango/rxjavaretrofitreview/Http/FilterSubscriber.java
+++ Rxjava&RetrofitReview/app/src/main/java/mango/mango/rxjavaretrofitreview/Http/FilterSubscriber.java
@@ -1,5 +1,7 @@
 package mango.mango.rxjavaretrofitreview.Http;
 
+import com.google.gson.JsonSyntaxException;
+
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 import java.util.concurrent.TimeoutException;
@@ -20,6 +22,10 @@ public abstract class FilterSubscriber<T> extends Subscriber<T> {
         if (e instanceof TimeoutException || e instanceof SocketTimeoutException
                 || e instanceof ConnectException){
             error = "超时了";
+        }else if (e instanceof JsonSyntaxException){
+            error = "Json格式出错了";
+            //假如导致这个异常触发的原因是服务器的问题，那么应该让服务器知道，所以可以在这里
+            //选择上传原始异常描述信息给服务器
         }else {
             error = e.getMessage();
         }
